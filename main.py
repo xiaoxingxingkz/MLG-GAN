2d358a47be99593eea8112efb1baf9f59a33a9f5
xiaoxingxingkz <106457371+xiaoxingxingkz@users.noreply.github.com>
2023-06-28 15:15:15 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from model.model import CoopNets
+from opts import opts
+import os
+
+
+
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+def main():
+    opt=opts().parse()
+    model=CoopNets(opt)
+    swich = 1
+    if swich == 0:
+        model.test()
+    elif swich == 1:
+        model.impute()
+    else:
+        model.train()
+
+if __name__=='__main__':
+    main()
+
